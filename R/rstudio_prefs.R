ed6f22377db2cebb680a149be49e9fbb15c36312
Garrick Aden-Buie <garrick@adenbuie.com>
2022-03-07 12:26:38 -0500

    document rstudio_prefs_v

--- R/rstudio_prefs.R
+++ R/rstudio_prefs.R
@@ -7,3 +7,16 @@
 #'
 #' @format `r rd_describe_schema(rstudio_prefs)`
 "rstudio_prefs"
+
+#' rstudio_prefs_v
+#'
+#' @description
+#' The RStudio User Preferences, their description and default value for
+#' released versions of RStudio.
+#'
+#' @source <https://github.com/rstudio/rstudio/raw/main/src/cpp/session/resources/schema/user-prefs-schema.json>
+#' @seealso [rstudio_prefs]
+#'
+#' @format A list with preferences from `r length(rstudio_prefs_v)` released
+#'   versions of RStudio.
+"rstudio_prefs_v"
