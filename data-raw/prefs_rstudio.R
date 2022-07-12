7f5a8df17a9629b3d1f617c395fb72569592f0bb
Garrick Aden-Buie <garrick@adenbuie.com>
2022-07-12 11:55:40 -0400

    Write JSON copy of `prefs_rstudio_v` for better diffs

--- data-raw/prefs_rstudio.R
+++ data-raw/prefs_rstudio.R
@@ -44,5 +44,15 @@ rstudio_prefs <-
 prefs_rstudio_v <- rstudio_prefs[setdiff(names(rstudio_prefs), "latest")]
 prefs_rstudio <- rstudio_prefs$latest
 
+# Write a JSON version of prefs for better diffs
+jsonlite::write_json(
+  prefs_rstudio_v,
+  here::here("data-raw/prefs_rstudio_v.json"),
+  pretty = TRUE,
+  auto_unbox = TRUE,
+  null = "null",
+  force = TRUE
+)
+
 saveRDS(prefs_rstudio, here::here("inst", "prefs_rstudio.rds"), version = 3)
 usethis::use_data(prefs_rstudio_v, overwrite = TRUE)
